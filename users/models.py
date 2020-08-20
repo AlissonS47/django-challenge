67a3333a4ec7989448dafd57ebb194650471fa74
Alisson Silva <tec.alisson47@gmail.com>
2020-08-19 21:35:57 -0300

    naver model(incomplete)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Naver(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    birthdate = models.DateField()
+    admission_date = models.DateField(auto_now_add=True)
+    job_role = models.CharField(max_length=20)
+    #projects
+
+    def __str__(self):
+        return self.name
